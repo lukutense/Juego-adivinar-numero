e7477ad8bffa334348b11c902c66eaa7bf8d82de
lukutense <lucaslmartinez0@gmail.com>
2022-06-14 16:41:45 -0300

    se agrego primer nivel pistas

--- main.js
+++ main.js
@@ -4,32 +4,27 @@ let submit=document.querySelector('#submit')
 let adivina=document.querySelector('#adivina')
 let numerosPrevios= document.querySelector('#numerosPrevios')
 let respuesta=document.querySelector('#respuesta')
-let numeroAleatorio= Math.round(Math.random() * (1 - 20) +20)
+
+let numeroAleatorio= Math.round(Math.random() * (1 - 100) +100)
 let numerosIngresados=[]
-let numeroComparado= 0
+
 
 comenzar.addEventListener('click',function(){
     game.classList.toggle('game')    
 })
 
-submit.addEventListener("click",function(event){
-    event.preventDefault()
+submit.addEventListener("click",(e)=>{
+    e.preventDefault()
     numerosIngresados.push(adivina.value)
     numerosPrevios.textContent=numerosIngresados
     
-    if(numerosIngresados.length === 10){
-        comparacion()
-
-        if(numeroComparado==numeroAleatorio){
-            respuesta.textContent=(`Ganaste, el numero ganador era ${numeroAleatorio}. Adivinaste, felicitaciones.`)
-        }else {
-            respuesta.textContent=(`Perdiste, el numero ganador era ${numeroAleatorio}. Mejor suerte la próxima vez.`)
-        }
+    if(adivina.value<numeroAleatorio){
+        respuesta.textContent=('el numero ganador es mayor, tenes x posiblidades mas')
+    } else if (adivina.value>numeroAleatorio){
+        respuesta.textContent=('el numero ganador es menor, tenes x posibilidades mas')
+    } else if(adivina.value==numeroAleatorio){
+        respuesta.textContent=(`Ganaste, el numero ganador era ${numeroAleatorio}. Adivinaste, felicitaciones.`)
+    } else {
+        respuesta.textContent=(`Perdiste, el numero ganador era ${numeroAleatorio}. Mejor suerte la próxima vez.`)  
     }
 })
-
-
-let comparacion=()=>{
-    numeroComparado=numerosIngresados.find(numero=>numero==numeroAleatorio)
-    return numeroComparado
-}
